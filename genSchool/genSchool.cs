4bc28b5ec0e689bebc4bb1534e40e3d42d64135b
SZYwayne <72900936+SZYwayne@users.noreply.github.com>
2024-03-28 18:47:54 +0800

    TJAD20240328

--- genSchool/genSchool.cs
+++ genSchool/genSchool.cs
@@ -23,6 +23,13 @@ namespace TJADSZY.genSchool
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            //pManager.AddNumberParameter("studentNumber", "SNum", "学生人数", GH_ParamAccess.list);
+            pManager.AddNumberParameter("plotRatio", "PR", "容积率(默认为1.5)", GH_ParamAccess.item, 1.5);
+            pManager.AddPointParameter("boundaryPoints", "nakedPts", "矩形的边界点", GH_ParamAccess.list);
+            pManager.AddCurveParameter("playgroundS", "PGs", "最小操场", GH_ParamAccess.list);
+            pManager.AddCurveParameter("playgroundM", "PGm", "中间操场", GH_ParamAccess.list);
+            pManager.AddCurveParameter("playgroundL", "PGl", "最大操场", GH_ParamAccess.list);
+            pManager.AddNumberParameter("dayFactor", "dayFactor", "日照系数", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -30,6 +37,8 @@ namespace TJADSZY.genSchool
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddNumberParameter("test", "test", "test", GH_ParamAccess.item);
+            pManager.AddCurveParameter("crv", "crv", "crv", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -38,6 +47,30 @@ namespace TJADSZY.genSchool
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            #region get inputs
+            List<Curve> PGs = new List<Curve>();
+            if (!DA.GetDataList("playgroundS", PGs)) return;
+            List<Curve> PGm = new List<Curve>();
+            if (!DA.GetDataList("playgroundM", PGs)) return;
+            List<Curve> PGl = new List<Curve>();
+            if (!DA.GetDataList("playgroundL", PGs)) return;
+            List<Point3d> nakedPts = new List<Point3d>();
+            if (!DA.GetDataList("boundaryPoints", nakedPts)) return;
+            double dayFactor = 0;
+            if (!DA.GetData("dayFactor", ref dayFactor)) return;
+            double PR = 0;
+            if (!DA.GetData("plotRatio", ref PR)) return;
+            #endregion
+
+            #region set student number
+            List<double> temp = utilities.area(utilities.polyline(nakedPts, true));
+            double totalArea = temp[0];
+            int SNum = Convert.ToInt32(totalArea * PR);
+            DA.SetData("test", SNum);
+            #endregion
+
+            //List<double> temp = utilities.area(utilities.polyline(nakedPts, true));
+            //DA.SetData("crv", tmpCrv);
         }
 
         /// <summary>
---
+++ genSchool/utilities.cs
@@ -0,0 +1,27 @@
+﻿using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.NodeInCode;
+
+namespace TJADSZY.genSchool
+{
+    internal class utilities
+    {
+        public static Curve polyline(List<Point3d> inputPts, bool isClosed)
+        {
+            var tmpFunc = Components.FindComponent("PolyLine").Delegate as dynamic;
+            return tmpFunc(inputPts, isClosed)[0];
+        }
+
+        public static  List<double> area(Curve inputBound)
+        {
+            var tmpFunc = Components.FindComponent("Area").Delegate as dynamic;
+            var tmpList = tmpFunc(inputBound)[0] as IList<object>;
+            List<double> outputList = tmpList.OfType<double>().ToList();
+            return outputList;
+        }
+    }
+}
